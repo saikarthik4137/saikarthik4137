bbd0e3410367b1d48fdd7c5d3ea8ded27d29e9f7
saikarthik4137 <mybestsai@gmail.com>
2025-07-27 17:22:58 +0530

    Merging

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, send_file, url_for
 from datetime import datetime
+from PIL import Image, ImageDraw, ImageFont
+import os
 
 app = Flask(__name__)
+OUTPUT_FOLDER = "static"
+os.makedirs(OUTPUT_FOLDER, exist_ok=True)
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
@@ -29,5 +33,52 @@ def index():
             result = {'error': 'Invalid input'}
     return render_template('index.html', result=result, today=today)
 
+@app.route("/poster-generator", methods=["GET", "POST"])
+def poster_generator():
+    image_path = None
+    if request.method == "POST":
+        gold = request.form["gold"]
+        silver = request.form["silver"]
+        today = datetime.now().strftime("%d/%m/%Y")
+
+        font_path = os.path.join(app.root_path, 'static', 'DejaVuSans-Bold.ttf')
+        font_path_regular = os.path.join(app.root_path, 'static', 'DejaVuSans.ttf')
+
+        title_font_small = ImageFont.truetype(font_path, 40)
+        text_font = ImageFont.truetype(font_path_regular, 35)
+        text_font_small = ImageFont.truetype(font_path_regular, 34)
+        bold_large_font = ImageFont.truetype(font_path, 52)
+
+        lines = [
+            ("Sri Annamalaiyar Jewels Manufacturers,Trichy", title_font_small),
+            ("Contact - 9965241162, 8144444588", text_font),
+            (f"Date - {today}", text_font),
+            (f"Gold 22 CT - ₹ {gold} /-", bold_large_font),
+            (f"Silver - ₹ {silver} /-", bold_large_font),
+            ("Wholesale jewels and Diamonds Available here at best prices.", text_font_small),
+            ("You're always welcome!!!", text_font)
+        ]
+
+        img = Image.new("RGB", (1080, 1080), color=(245, 231, 220))
+        draw = ImageDraw.Draw(img)
+
+        y = 180
+        for line, font in lines:
+            text_width = draw.textlength(line, font=font)
+            draw.text(((1080 - text_width) / 2, y), line, fill="black", font=font)
+            y += font.getbbox(line)[3] + 30
+
+        filename = f"poster_{datetime.now().strftime('%Y%m%d_%H%M%S')}.jpg"
+        path = os.path.join(OUTPUT_FOLDER, filename)
+        img.save(path)
+        image_path = path
+
+    return render_template("poster.html", image_path=image_path)
+
+@app.route("/download/<filename>")
+def download(filename):
+    return send_file(os.path.join(OUTPUT_FOLDER, filename), as_attachment=True)
+
 if __name__ == '__main__':
     app.run(debug=True)
+    
