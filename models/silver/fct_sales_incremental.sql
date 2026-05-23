ca96c00d64c72e32453dc977f02317e481d08af7
sai.karthik7169@gmail.com <sai.karthik7169@gmail.com>
2026-05-23 19:26:00 +0000

    DD

--- models/silver/fct_sales_incremental.sql
+++ models/silver/fct_sales_incremental.sql
@@ -1,7 +1,34 @@
 {{ config(
-    materialized='table'
+    materialized='incremental',
+    unique_key='id'
 ) }}
 
-SELECT *
+SELECT
+
+    id,
+    name,
+    amount,
+    created_at,
+    is_active,
+    item_id,
+    price,
+    total_value,
+
+    loaded_at,
+    updated_at,
+    pipeline_run_at,
+
+    raw_loaded_at,
+    rn
 
 FROM {{ ref('stg_sales_clean') }}
+
+{% if is_incremental() %}
+
+WHERE raw_loaded_at >
+(
+    SELECT COALESCE(MAX(raw_loaded_at), '1900-01-01'::TIMESTAMP)
+    FROM {{ this }}
+)
+
+{% endif %}
