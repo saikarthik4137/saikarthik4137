834692f6a550f05d0da6b908149e8b58f58b1a78
sai.karthik7169@gmail.com <sai.karthik7169@gmail.com>
2026-05-24 10:00:16 +0000

    ss

--- models/silver/fct_sales_incremental.sql
+++ models/silver/fct_sales_incremental.sql
@@ -3,31 +3,64 @@
     unique_key='id'
 ) }}
 
-SELECT
+WITH source_data AS (
 
+    SELECT
+        id,
+        name,
+        amount,
+        created_at,
+        is_active,
+        unexpected,
+        item_id,
+        price,
+        total_value,
+        loaded_at,
+        updated_at,
+        pipeline_run_at,
+        raw_loaded_at,
+        file_name,
+
+        ROW_NUMBER() OVER (
+            PARTITION BY id, item_id
+            ORDER BY created_at DESC
+        ) AS rn
+
+    FROM {{ ref('stg_sales_clean') }}
+
+),
+
+final_data AS (
+
+    SELECT *
+    FROM source_data
+    WHERE rn = 1
+
+)
+
+SELECT
     id,
     name,
     amount,
     created_at,
     is_active,
+    unexpected,
     item_id,
     price,
     total_value,
-
     loaded_at,
     updated_at,
     pipeline_run_at,
-
     raw_loaded_at,
-    rn
+    file_name
 
-FROM {{ ref('stg_sales_clean') }}
+FROM final_data
 
 {% if is_incremental() %}
 
-WHERE raw_loaded_at >
+WHERE updated_at >
 (
-    SELECT COALESCE(MAX(raw_loaded_at), '1900-01-01'::TIMESTAMP)
+    SELECT COALESCE(MAX(updated_at), '1900-01-01')
     FROM {{ this }}
 )
 
