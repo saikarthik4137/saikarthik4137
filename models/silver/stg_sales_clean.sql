ca4a35a4c47df5a1b5ada119ec5ba9d26490f530
sai.karthik7169@gmail.com <sai.karthik7169@gmail.com>
2026-05-24 09:58:11 +0000

    ss

--- models/silver/stg_sales_clean.sql
+++ models/silver/stg_sales_clean.sql
@@ -1,43 +1,56 @@
-{{ config(
-    materialized='table'
-) }}
+{{ config(materialized='table') }}
 
 WITH clean_data AS (
 
-    SELECT
-
-        id,
-        TRIM(name) AS name,
-        amount,
-        created_at,
-        is_active,
-        item_id,
-        price,
-        total_value,
-
-        CURRENT_TIMESTAMP() AS loaded_at,
-        CURRENT_TIMESTAMP() AS updated_at,
-        CURRENT_TIMESTAMP() AS pipeline_run_at,
-
-        raw_loaded_at
+    SELECT *
 
     FROM {{ ref('stg_raw_json_data') }}
+
+    WHERE id IS NOT NULL
+      AND name IS NOT NULL
+      AND TRIM(name) <> ''
+
+      AND amount IS NOT NULL
+      AND amount >= 0
+
+      AND created_at IS NOT NULL
+
+      AND item_id IS NOT NULL
+
+      AND price IS NOT NULL
+      AND price >= 0
+
+      AND total_value IS NOT NULL
+      AND total_value >= 0
 
 ),
 
 dedup AS (
 
     SELECT *,
-
         ROW_NUMBER() OVER (
             PARTITION BY id, item_id
-            ORDER BY raw_loaded_at DESC
+            ORDER BY created_at DESC
         ) AS rn
-
     FROM clean_data
+
 )
 
-SELECT *
+SELECT
+    id,
+    name,
+    amount,
+    created_at,
+    is_active,
+    unexpected,
+    item_id,
+    price,
+    total_value,
+    loaded_at,
+    updated_at,
+    pipeline_run_at,
+    raw_loaded_at,
+    file_name
 
 FROM dedup
 WHERE rn = 1
